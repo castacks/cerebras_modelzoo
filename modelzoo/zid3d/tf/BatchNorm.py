91559b8b74e5a38c1f611be7345135255fc36cdc
yaoyuh <yaoyuh@andrew.cmu.edu>
2022-12-11 06:35:31 -0500

    Work on local comptuer.

--- modelzoo/zid3d/tf/BatchNorm.py
+++ modelzoo/zid3d/tf/BatchNorm.py
@@ -1,10 +1,23 @@
 
-from tensorflow.keras.layers import BatchNormalization
+import tensorflow as tf
+
+from keras import constraints
+from keras import initializers
+from keras import regularizers
+from keras.dtensor import utils
+from keras.engine.base_layer import Layer
+from keras.utils import tf_utils
+
+from keras import backend
+from keras.engine.input_spec import InputSpec
+from keras.utils import control_flow_util
 
 from modelzoo.common.tf.layers.BaseLayer import BaseLayer
 from modelzoo.common.tf.layers.utils import boundary_cast, summary_layer
 
 class BatchNormalizationLayer(BaseLayer):
+    _USE_V2_BEHAVIOR = True
+    
     def __init__(
         self,
         axis=-1,
@@ -29,32 +42,319 @@ class BatchNormalizationLayer(BaseLayer):
             boundary_casting, tf_summary, **kwargs
         )
         
-        self.layer = BatchNormalization(
-            axis=axis,
-            momentum=momentum,
-            epsilon=epsilon,
-            center=center,
-            scale=scale,
-            beta_initializer=beta_initializer,
-            gamma_initializer=gamma_initializer,
-            moving_mean_initializer=moving_mean_initializer,
-            moving_variance_initializer=moving_variance_initializer,
-            beta_regularizer=beta_regularizer,
-            gamma_regularizer=gamma_regularizer,
-            beta_constraint=beta_constraint,
-            gamma_constraint=gamma_constraint,
-            trainable=trainable,
-            name=self.name,
-            dtype=self.dtype_policy,
+        if isinstance(axis, (list, tuple)):
+            self.axis = axis[:]
+        elif isinstance(axis, int):
+            self.axis = axis
+        else:
+            raise TypeError(
+                "Expected an int or a list/tuple of ints for the "
+                "argument 'axis', but received: %r" % axis
+            )
+        self.momentum = momentum
+        self.epsilon = epsilon
+        self.center = center
+        self.scale = scale
+        self.beta_initializer = initializers.get(beta_initializer)
+        self.gamma_initializer = initializers.get(gamma_initializer)
+        self.moving_mean_initializer = initializers.get(moving_mean_initializer)
+        self.moving_variance_initializer = initializers.get(
+            moving_variance_initializer
         )
+        self.beta_regularizer = regularizers.get(beta_regularizer)
+        self.gamma_regularizer = regularizers.get(gamma_regularizer)
+        self.beta_constraint = constraints.get(beta_constraint)
+        self.gamma_constraint = constraints.get(gamma_constraint)
         
+        self.fused = False
+        
+        self.trainable = trainable
+    
+    @property
+    def _param_dtype(self):
+        # Raise parameters of fp16 batch norm to fp32
+        if self.dtype == tf.float16 or self.dtype == tf.bfloat16:
+            return tf.float32
+        else:
+            return self.dtype or tf.float32
+    
+    def build(self, input_shape):
+        self.axis = tf_utils.validate_axis(self.axis, input_shape)
+        input_shape = tf.TensorShape(input_shape)
+        rank = input_shape.rank
+
+        axis_to_dim = {x: input_shape.dims[x].value for x in self.axis}
+        for x in axis_to_dim:
+            if axis_to_dim[x] is None:
+                raise ValueError(
+                    "Input has undefined `axis` dimension. Received input "
+                    f"with shape {tuple(input_shape)} "
+                    f"and axis={tuple(self.axis)}"
+                )
+        self.input_spec = InputSpec(ndim=rank, axes=axis_to_dim)
+
+        if len(axis_to_dim) == 1:
+            # Single axis batch norm (most common/default use-case)
+            param_shape = (list(axis_to_dim.values())[0],)
+        else:
+            # Parameter shape is the original shape but with 1 in all non-axis
+            # dims
+            param_shape = [
+                axis_to_dim[i] if i in axis_to_dim else 1 for i in range(rank)
+            ]
+
+        self._param_shape = param_shape
+        if self.scale:
+            self.gamma = self.add_weight(
+                name="gamma",
+                shape=param_shape,
+                dtype=self._param_dtype,
+                initializer=self.gamma_initializer,
+                regularizer=self.gamma_regularizer,
+                constraint=self.gamma_constraint,
+                trainable=True,
+                experimental_autocast=False,
+            )
+        else:
+            self.gamma = None
+
+        if self.center:
+            self.beta = self.add_weight(
+                name="beta",
+                shape=param_shape,
+                dtype=self._param_dtype,
+                initializer=self.beta_initializer,
+                regularizer=self.beta_regularizer,
+                constraint=self.beta_constraint,
+                trainable=True,
+                experimental_autocast=False,
+            )
+        else:
+            self.beta = None
+
+        try:
+            # Disable variable partitioning when creating the moving mean and
+            # variance
+            if hasattr(self, "_scope") and self._scope:
+                partitioner = self._scope.partitioner
+                self._scope.set_partitioner(None)
+            else:
+                partitioner = None
+            self.moving_mean = self.add_weight(
+                name="moving_mean",
+                shape=param_shape,
+                dtype=self._param_dtype,
+                initializer=self.moving_mean_initializer,
+                synchronization=tf.VariableSynchronization.ON_READ,
+                trainable=False,
+                aggregation=tf.VariableAggregation.MEAN,
+                experimental_autocast=False,
+            )
+
+            self.moving_variance = self.add_weight(
+                name="moving_variance",
+                shape=param_shape,
+                dtype=self._param_dtype,
+                initializer=self.moving_variance_initializer,
+                synchronization=tf.VariableSynchronization.ON_READ,
+                trainable=False,
+                aggregation=tf.VariableAggregation.MEAN,
+                experimental_autocast=False,
+            )
+        finally:
+            if partitioner:
+                self._scope.set_partitioner(partitioner)
+        self.built = True
+    
+    def _assign_moving_average(self, variable, value, momentum, inputs_size):
+        def calculate_update_delta():
+            decay = tf.convert_to_tensor(1.0 - momentum, name="decay")
+            if decay.dtype != variable.dtype.base_dtype:
+                decay = tf.cast(decay, variable.dtype.base_dtype)
+            update_delta = (variable - tf.cast(value, variable.dtype)) * decay
+            if inputs_size is not None:
+                update_delta = tf.where(
+                    inputs_size > 0,
+                    update_delta,
+                    backend.zeros_like(update_delta),
+                )
+            return update_delta
+
+        with backend.name_scope("AssignMovingAvg") as scope:
+            if tf.compat.v1.executing_eagerly_outside_functions():
+                return variable.assign_sub(calculate_update_delta(), name=scope)
+            else:
+                with tf.compat.v1.colocate_with(variable):
+                    return tf.compat.v1.assign_sub(
+                        variable, calculate_update_delta(), name=scope
+                    )
+
+    def _assign_new_value(self, variable, value):
+        with backend.name_scope("AssignNewValue") as scope:
+            if tf.compat.v1.executing_eagerly_outside_functions():
+                return variable.assign(value, name=scope)
+            else:
+                with tf.compat.v1.colocate_with(variable):
+                    return tf.compat.v1.assign(variable, value, name=scope)
+    
+    def _calculate_mean_and_var(self, inputs, reduction_axes, keep_dims):
+        return tf.nn.moments(inputs, reduction_axes, keepdims=keep_dims)
+    
+    def _moments(self, inputs, reduction_axes, keep_dims):
+        mean, variance = self._calculate_mean_and_var(
+            inputs, reduction_axes, keep_dims
+        )
+        return mean, variance
+
+    def _get_training_value(self, training=None):
+        if training is None:
+            training = backend.learning_phase()
+        if self._USE_V2_BEHAVIOR:
+            if isinstance(training, int):
+                training = bool(training)
+            if not self.trainable:
+                # When the layer is not trainable, it overrides the value passed
+                # from model.
+                training = False
+        return training
+    
+    def layer_call(self, inputs, **kwargs):
+        inputs = tf.cast(inputs, self.compute_dtype)
+        training = self._get_training_value(training)
+        # Determine a boolean value for `training`: could be True, False, or
+        # None.
+        training_value = control_flow_util.constant_value(training)
+
+        inputs_dtype = inputs.dtype.base_dtype
+        if inputs_dtype in (tf.float16, tf.bfloat16):
+            # Do all math in float32 if given 16-bit inputs for numeric
+            # stability.  In particular, it's very easy for variance to overflow
+            # in float16 and for safety we also choose to cast bfloat16 to
+            # float32.
+            inputs = tf.cast(inputs, tf.float32)
+
+        # Compute the axes along which to reduce the mean / variance
+        input_shape = inputs.shape
+        ndims = len(input_shape)
+        reduction_axes = [i for i in range(ndims) if i not in self.axis]
+
+        # Broadcasting only necessary for single-axis batch norm where the axis
+        # is not the last dimension
+        broadcast_shape = [1] * ndims
+        broadcast_shape[self.axis[0]] = input_shape.dims[self.axis[0]].value
+
+        def _broadcast(v):
+            if (
+                v is not None
+                and len(v.shape) != ndims
+                and reduction_axes != list(range(ndims - 1))
+            ):
+                return tf.reshape(v, broadcast_shape)
+            return v
+
+        scale, offset = _broadcast(self.gamma), _broadcast(self.beta)
+
+        def _compose_transforms(scale, offset, then_scale, then_offset):
+            if then_scale is not None:
+                scale *= then_scale
+                offset *= then_scale
+            if then_offset is not None:
+                offset += then_offset
+            return (scale, offset)
+
+        if training_value == False:  # noqa: E712
+            mean, variance = self.moving_mean, self.moving_variance
+        else:
+            # Some of the computations here are not necessary when
+            # training==False but not a constant. However, this makes the code
+            # simpler.
+            keep_dims = (
+                len(self.axis) > 1
+            )
+            mean, variance = self._moments(
+                tf.cast(inputs, self._param_dtype),
+                reduction_axes,
+                keep_dims=keep_dims,
+            )
+
+            moving_mean = self.moving_mean
+            moving_variance = self.moving_variance
+
+            mean = control_flow_util.smart_cond(
+                training,
+                lambda: mean,
+                lambda: tf.convert_to_tensor(moving_mean),
+            )
+            variance = control_flow_util.smart_cond(
+                training,
+                lambda: variance,
+                lambda: tf.convert_to_tensor(moving_variance),
+            )
+
+            new_mean, new_variance = mean, variance
+
+            input_batch_size = None
+
+            def _do_update(var, value):
+                """Compute the updates for mean and variance."""
+                return self._assign_moving_average(
+                    var, value, self.momentum, input_batch_size
+                )
+
+            def mean_update():
+                true_branch = lambda: _do_update(self.moving_mean, new_mean)
+                false_branch = lambda: self.moving_mean
+                return control_flow_util.smart_cond(
+                    training, true_branch, false_branch
+                )
+
+            def variance_update():
+                """Update the moving variance."""
+
+                true_branch = lambda: _do_update(
+                    self.moving_variance, new_variance
+                )
+
+                false_branch = lambda: self.moving_variance
+                return control_flow_util.smart_cond(
+                    training, true_branch, false_branch
+                )
+
+            self.add_update(mean_update)
+            self.add_update(variance_update)
+
+        mean = tf.cast(mean, inputs.dtype)
+        variance = tf.cast(variance, inputs.dtype)
+        if offset is not None:
+            offset = tf.cast(offset, inputs.dtype)
+        if scale is not None:
+            scale = tf.cast(scale, inputs.dtype)
+        outputs = tf.nn.batch_normalization(
+            inputs,
+            _broadcast(mean),
+            _broadcast(variance),
+            offset,
+            scale,
+            self.epsilon,
+        )
+        if inputs_dtype in (tf.float16, tf.bfloat16):
+            outputs = tf.cast(outputs, inputs_dtype)
+
+        # If some components of the shape got lost due to adjustments, fix that.
+        outputs.set_shape(input_shape)
+
+        return outputs
+    
     def call(self, inputs, **kwargs):
         if self.boundary_casting:
             inputs = boundary_cast(inputs)
             
-        output = self.layer(inputs)
+        output = self.layer_call(inputs, kwargs)
         
         if self.tf_summary:
             output = summary_layer(output)
         
         return output
+    
+    def compute_output_shape(self, input_shape):
+        return input_shape
